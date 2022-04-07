430c722da263a11d0729b3ae01ea5651d7543fd8
Ryan Nathan Mulyadi <r.mulyadi@student.unsw.edu.au>
2022-04-07 14:36:33 +1000

    Replace apostrophes and double quote with html escape characters

--- pages/natas/natas2.tsx
+++ pages/natas/natas2.tsx
@@ -4,7 +4,7 @@ const natas2 = () => {
       I noticed a picture of a single pixel, first I thought,
       perhaps it is hidden in this dubious one pixel picture.
       I tried inspecting its contents using vim and xxd without
-      much luck. Perhaps the hint that there's nothing in this
+      much luck. Perhaps the hint that there&apos;s nothing in this
       page is a little more accurate. So I noticed that the pixel
       was located in the files folder, so I added that to the 
       website link and amazingly, a text file was there.
--- pages/natas/natas3.tsx
+++ pages/natas/natas3.tsx
@@ -2,11 +2,11 @@ const natas3 = () => {
   return (
     <div>
       <p>
-        I couldn't find anything in this webpage, the document
+        I couldn&apos;t find anything in this webpage, the document
         looks quite clean except a comment about how even Google
-        can't find it. So I searched "how to hide something in website programming from google"
-        and the <a href='https://www.searchenginejournal.com/google-explains-how-to-hide-a-website-from-search-results/428341/'>first result</a> gave me several ways. The first method is to use a password, I don't
-        see any links which requires password in the page, so it can't be that one. The second method
+        can&apos;t find it. So I searched &quot;how to hide something in website programming from google&quot;
+        and the <a href='https://www.searchenginejournal.com/google-explains-how-to-hide-a-website-from-search-results/428341/'>first result</a> gave me several ways. The first method is to use a password, I don&apos;t
+        see any links which requires password in the page, so it can&apos;t be that one. The second method
         mention blocking it from web crawlers by adding a robots.txt. So I appended robots.txt
         to the web address, and voila, was greeted with the text:
       </p>

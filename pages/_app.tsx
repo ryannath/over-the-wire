93eafce84d01de80200ae0782f793948fb39242a
Ryan Nathan Mulyadi <r.mulyadi@student.unsw.edu.au>
2022-04-07 14:31:24 +1000

    Allow for per-page layout in app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '/styles/globals.scss'
+import type { AppLayoutProps, AppProps } from 'next/app'
 import MainLayout from '../components/layouts/MainLayout'
+import { ReactNode } from 'react'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <MainLayout>
-      <Component {...pageProps} />
-    </MainLayout>
-  )
+function MyApp({ Component, pageProps }: AppLayoutProps) {
+
+  const getLayout = Component.getLayout || ((page: ReactNode) => <MainLayout>{page}</MainLayout>)
+  let content = <Component {...pageProps}/>
+  if (Component.getLayout) {
+    content = Component.getLayout(content);
+  }
+  return <MainLayout>{content}</MainLayout>
 }
 
 export default MyApp

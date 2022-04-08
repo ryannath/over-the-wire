9604b6fd60db6f27a9511ddd05ac7ea740b034d2
Ryan Nathan Mulyadi <r.mulyadi@student.unsw.edu.au>
2022-04-08 23:00:07 +1000

    Added more depth and reflection to blog entries natas0-2

--- pages/natas/natas2.tsx
+++ pages/natas/natas2.tsx
@@ -1,13 +1,75 @@
+import Image from "next/image"
+import PageNav from "../../components/elements/PageNav/PageNav"
+import devToolsPic from '/public/natas2.png'
+import directoryPic from '/public/natas2-2.png'
+
 const natas2 = () => {
   return (
     <div>
-      I noticed a picture of a single pixel, first I thought,
-      perhaps it is hidden in this dubious one pixel picture.
-      I tried inspecting its contents using vim and xxd without
-      much luck. Perhaps the hint that there&apos;s nothing in this
-      page is a little more accurate. So I noticed that the pixel
-      was located in the files folder, so I added that to the 
-      website link and amazingly, a text file was there.
+      <h1>Natas 2</h1>
+      <section>
+        <h2>Experience</h2>
+        <p>
+          I noticed a picture of a single pixel, first I thought,
+          perhaps it is hidden in this dubious one pixel picture.
+          I tried inspecting its contents using vim and xxd without
+          much luck. Perhaps the hint that there&apos;s nothing in this
+          page is true.
+        </p>
+        <p>
+          Then I thought, this pixel picture is an indicator that there might
+          be other files in the web server, and potentially one with the
+          flag we are looking for. I attempted to inspect the list of files
+          on the developer tools, however the developer tools didn&apos;t show
+          any other files worth noting, see the picture below.
+        </p>
+        <div>
+          <Image
+            src={devToolsPic}
+            width={devToolsPic.width * 0.8}
+            height={devToolsPic.height * 0.8}
+            alt='Image showing file hierarchy shown by developer tools'
+            layout='intrinsic'
+          />
+        </div>
+        <p>
+          I did however notice that the pixel photo was located in the
+          &quot;files&quot; directory. So I tried appending <code>/files</code> to the
+          url, and there it was, <code>users.txt</code>. I simply clicked this file
+          and it displayed the password for the next level.
+        </p>
+        <div>
+          <Image
+            src={directoryPic}
+            width={directoryPic.width * 0.6}
+            height={directoryPic.height * 0.6}
+            alt='The directory page shown after /files was appended, listed as
+            one of the file is users.txt'
+            layout='intrinsic'
+          />
+        </div>
+      </section>
+      <section>
+        <h2>Reflection</h2>
+        <p>
+          This exercise was a little bit harder than before as I had no previous
+          knowledge on what I am supposed to do. The website itself seems very clean and
+          even the folder hierarchy in the developer tools is clean. I&apos;ve now
+          learnt that while files that are not linked to by the website is not shown
+          on the developer tools, it is still possible to access them. This seems
+          particularly dangerous. If a developer were simply checking if any
+          potentially sensitive files were put in this public directory, the developer
+          tools won&apos;t show it.
+        </p>
+        <p>
+          The level also introduced that webpages may comprised of files and directories
+          as well. So not only is something written in the html document is public,
+          every other client side files are also publically available.
+        </p>
+      </section>
+
+
+      <PageNav link1='natas1' link2='natas3' />
     </div>
   )
 }

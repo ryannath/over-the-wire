883d179bb662494128ad30bd3fcff5afc5c7f9e3
Ryan Nathan Mulyadi <r.mulyadi@student.unsw.edu.au>
2022-04-08 22:46:21 +1000

    Added description to what natas teaches and change IndexList to NatasIndexList

--- pages/natas/index.tsx
+++ pages/natas/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head"
-import Link from "next/link"
-import IndexList from "../../components/elements/IndexList/IndexList";
+import NatasIndexList from "../../components/elements/IndexList/NatasIndexList";
 import MainTitle from "../../components/elements/MainTitle/MainTitle";
+import PageNav from "../../components/elements/PageNav/PageNav";
 
 const natas = () => {
   return (
@@ -9,10 +9,21 @@ const natas = () => {
       <Head>
         <title>Natas</title>
       </Head>
-      <MainTitle>
-        Natas
-      </MainTitle>
-      <IndexList path='/natas/' text='natas' range={6}/>
+      <MainTitle>Natas</MainTitle>
+      <p>
+        As stated by the description on the OverTheWire page,
+        natas revolves around serverside web security. That is,
+        the server which websites are hosted on. Many of these vulnerabilities
+        involve features popularly implemented on websites. Beginner
+        website developers will find this very useful to keep in mind
+        so that they will be able to develop safer websites. These
+        common features may include, taking user input to search in
+        server directory, uploading files, searching for entries
+        in the website, etc.
+      </p>
+      <h2>List of levels</h2>
+      <NatasIndexList />
+      <PageNav link1='' link2='natas/natas0'/>
     </>
       
   )

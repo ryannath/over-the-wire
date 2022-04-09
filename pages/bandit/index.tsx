9ad36d5fef40319b18d15e635416239ce7b71cf6
Ryan Nathan Mulyadi <r.mulyadi@student.unsw.edu.au>
2022-04-09 20:21:34 +1000

    Added descriptions for bandit index page

--- pages/bandit/index.tsx
+++ pages/bandit/index.tsx
@@ -1,22 +1,31 @@
 import Head from "next/head"
-import Link from "next/link"
-import IndexList from "../../components/elements/IndexList/IndexList"
+import BanditIndexList from "../../components/elements/IndexList/BanditIndexList";
 import MainTitle from "../../components/elements/MainTitle/MainTitle";
+import PageNav from "../../components/elements/PageNav/PageNav";
 
-const numPages = 1;
 
 const bandit = () => {
   return (
     <>
-    <Head>
-      <title>Bandit</title>
-    </Head>
+      <Head>
+        <title>Bandit</title>
+      </Head>
       <MainTitle>Bandit</MainTitle>
-      <IndexList
-        path='/bandit/'
-        text='bandit'
-        range={numPages}
-      />
+      <p>
+        While bandit is aimed at absolute beginners, there are several key concepts
+        that are useful to learn for other wargames.
+      </p>
+      <p>
+        The lower level of bandit
+        teaches participants about many useful shell commands. These commands could
+        then be useful when the participant wishes to interact with Unix and other
+        Unix based system. For example, during <em>Natas</em>, another wargame,
+        one of the vulnerability requires the user to run a shell command remotely
+        to obtain the password located in the server.
+      </p>
+      <h2>List of levels</h2>
+      <BanditIndexList />
+      <PageNav link1='' link2='bandit/bandit0'/>
     </>
   );
 }

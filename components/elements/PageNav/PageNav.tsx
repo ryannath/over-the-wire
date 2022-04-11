d20dbf75ff1ecd99f2a5d4dba0f1cee19bbe8d83
Ryan Nathan Mulyadi <r.mulyadi@student.unsw.edu.au>
2022-04-12 00:05:29 +1000

    Add tool tips to PageNav

--- components/elements/PageNav/PageNav.tsx
+++ components/elements/PageNav/PageNav.tsx
@@ -5,10 +5,10 @@ const PageNav = ({link1, link2}: {link1: string, link2: string}) => {
   return (
     <div className={styles.pageNav}>
       <div >
-        {link1? <Link href={link1}><a className={styles.navButton}>◀</a></Link> : ''}
+        {link1? <Link href={link1}><a title='Previous page' className={styles.navButton}>◀</a></Link> : ''}
       </div>
       <div>
-        {link2? <Link href={link2}><a className={styles.navButton}>▶</a></Link> : ''}
+        {link2? <Link href={link2}><a title='Next page' className={styles.navButton}>▶</a></Link> : ''}
       </div>
     </div>
   )

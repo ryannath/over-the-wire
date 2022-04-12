481af95de11662bf33f0471175a1e8332466ef55
Ryan Nathan Mulyadi <r.mulyadi@student.unsw.edu.au>
2022-04-12 11:40:14 +1000

    Accordion like component for displaying spoiler content

--- components/elements/SpoilerKey/SpoilerKey.tsx
+++ components/elements/SpoilerKey/SpoilerKey.tsx
@@ -1,13 +1,28 @@
-import { ReactNode } from "react"
-import CodeBlock from "../CodeBlock/CodeBlock"
+import styles from './SpoilerKey.module.scss'
+import { Prism } from 'react-syntax-highlighter'
+import type { SyntaxHighlighterProps } from "react-syntax-highlighter"
+import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism'
+import { ReactNode, useState } from 'react'
 
-const SpoilerKey = ({children}: {children: ReactNode}) => {
+
+const SpoilerKey = ({ children }: { children: ReactNode }) => {
+  const [hide, setHidden] = useState(false);
+
   return (
     <section>
       <h2>Key</h2>
-      <CodeBlock language=''>
-        {children}
-      </CodeBlock>
+      <div className={styles.codeBlock}>
+        <div className={styles.spoilerToggler}>
+          <button onClick={() => setHidden(!hide)}>Click to Reveal</button>
+        </div>
+        <div className={styles.spoilerContainer}>
+          <div className={`${styles.spoiler} ${hide ? styles.show : ''}`}>
+            <Prism language='' style={dracula} customStyle={{ margin: 0 }}>
+              {children}
+            </Prism>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }

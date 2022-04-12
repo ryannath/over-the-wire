37def82407a83e3d6f72c611294cf3380e12bcc6
Ryan Nathan Mulyadi <r.mulyadi@student.unsw.edu.au>
2022-04-12 11:37:33 +1000

    Implemented replaceable ciphers

--- components/elements/Cipher/Cipher.tsx
+++ components/elements/Cipher/Cipher.tsx
@@ -1,32 +1,49 @@
-import { RefObject, useEffect, useState } from "react"
+import { useState } from "react";
 import CaesarCipher from "../CaesarCipher/CaesarCipher";
-import styles from './Cipher.module.scss'
+import SubCipher from "../SubCipher/SubCipher";
+import styles from './Cipher.module.scss';
 
 const Cipher = () => {
-  const [cipher, setCipher] = useState('');
+  const [cipher, setCipher] = useState(0);
   const [decodedText, setDecodedText] = useState('');
   const [encodedText, setEncodedText] = useState('');
   const [lastSelected, setLastSelected] = useState(0);
 
+  const attributes = {
+    'setEncodedText': setEncodedText,
+    'setDecodedText': setDecodedText,
+    'encodedText': encodedText,
+    'decodedText': decodedText,
+    'lastSelected': lastSelected
+  }
+
+  let cipherElement = null;
+  switch (cipher) {
+    case 0:
+      cipherElement = <CaesarCipher {...attributes} />
+      break;
+    case 1:
+      cipherElement = <SubCipher {...attributes} />
+      break;
+    default:
+      cipherElement = <CaesarCipher {...attributes} />;
+  }
+
   return (
     <div>
+      <div className={styles.dropDownContainer}>
+        <select className={styles.dropDown} value={cipher} onChange={(e) => setCipher(parseInt(e.target.value))}>
+          <option value={0}>Caesar Cipher</option>
+          <option value={1}>Substitution Cipher</option>
+        </select>
+      </div>
 
-      <select className={styles.dropDown}>
-        <option>Caesar Cipher</option>
-      </select>
-
-      <CaesarCipher
-        setEncodedText={setEncodedText}
-        setDecodedText={setDecodedText}
-        encodedText={encodedText}
-        decodedText={decodedText}
-        lastSelected={lastSelected}
-      />
+      {cipherElement}
 
       <div className={styles.container}>
         <label>Plain text / decoded</label>
         <textarea
-          className={`${styles.textField} ${!lastSelected? styles.selected : ''}`}
+          className={`${styles.textField} ${!lastSelected ? styles.selected : ''}`}
           value={decodedText}
           onSelect={() => setLastSelected(0)}
           onChange={(e) => setDecodedText(e.target.value)}
@@ -36,7 +53,7 @@ const Cipher = () => {
       <div className={styles.container}>
         <label>Encrypted</label>
         <textarea
-          className={`${styles.textField} ${lastSelected? styles.selected : ''}`}
+          className={`${styles.textField} ${lastSelected ? styles.selected : ''}`}
           value={encodedText}
           onSelect={() => setLastSelected(1)}
           onChange={(e) => setEncodedText(e.target.value)}

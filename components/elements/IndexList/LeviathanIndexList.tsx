8c5a9fd3fec14bc8a76a93046d5604c18f2732f7
Ryan Nathan Mulyadi <r.mulyadi@student.unsw.edu.au>
2022-04-12 12:07:03 +1000

    Add index list for leviathan and narnia

---
+++ components/elements/IndexList/LeviathanIndexList.tsx
@@ -0,0 +1,10 @@
+import IndexList from "./IndexList"
+
+const numPages = 3;
+
+const LeviathanIndexList = () => {
+  return (
+    <IndexList path='/leviathan/' text='leviathan' range={numPages}/>
+  )
+}
+export default LeviathanIndexList
---
+++ components/elements/IndexList/NarniaIndexList.tsx
@@ -0,0 +1,10 @@
+import IndexList from "./IndexList"
+
+const numPages = 3;
+
+const NarniaIndexList = () => {
+  return (
+    <IndexList path='/narnia/' text='narnia' range={numPages}/>
+  )
+}
+export default NarniaIndexList

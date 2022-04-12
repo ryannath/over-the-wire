525a7f851ac71f88c7971a9162b568a0badc7e61
Ryan Nathan Mulyadi <r.mulyadi@student.unsw.edu.au>
2022-04-12 12:18:25 +1000

    Add path prefix

--- components/elements/BlogImage/BlogImage.tsx
+++ components/elements/BlogImage/BlogImage.tsx
@@ -1,9 +1,11 @@
 import styles from './BlogImage.module.scss'
 
+const pathPrefix = '/over-the-wire';
+
 const BlogImage = ({src, alt, width=600}: {src: string, alt: string, width?: number}) => {
   return (
     <div className={styles.imgContainer}>
-      <img src={src} alt={alt} width={width}/>
+      <img src={pathPrefix + src} alt={alt} width={width}/>
     </div>
   )
 }
--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/over-the-wire',
+  assetPrefix: '/over-the-wire',
 }
 
 module.exports = nextConfig

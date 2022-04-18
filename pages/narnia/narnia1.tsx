e8f1cda1ff82aa182121f2a5e1631c386ab0f1cb
Ryan Nathan Mulyadi <r.mulyadi@student.unsw.edu.au>
2022-04-18 10:23:06 +1000

    Overhauled Narnia1 blog

--- pages/narnia/narnia1.tsx
+++ pages/narnia/narnia1.tsx
@@ -1,3 +1,4 @@
+import BlogImage from "../../components/elements/BlogImage/BlogImage"
 import CodeBlock from "../../components/elements/CodeBlock/CodeBlock"
 import PageNav from "../../components/elements/PageNav/PageNav"
 import SpoilerKey from "../../components/elements/SpoilerKey/SpoilerKey"
@@ -38,15 +39,93 @@ return 0;
           I could understand so far that we must set the environment variable to
           something executable, however, I did not know how to proceed. Searching
           setting environment variable vulnerabilities led to a <a href="https://owasp.org/www-community/attacks/Buffer_Overflow_via_Environment_Variables">page in owasp.org</a>.
-          
+          This seem tangentially related, however, it uses environment variable to create a buffer
+          overflow rather than creating an executable function. Lastly, I was forced to
+          find hints regarding this level. I learnt that we were supposed to use something
+          called shellcode.
+        </p>
+        <p>
+          As the creation of shellcodes could be quite difficult especially in relation to
+          removing null bytes, we can use a ready shellcode taken from websites such as <a href='http://shell-storm.org/shellcode/'>exploit-db</a>. If we analyse the code of narnia0,
+          it calls <code>execve</code> with <code>/bin/sh</code> as arguments, so searching
+          for a shellcode that does those, I found one from <a href='https://www.exploit-db.com/exploits/39160'>exploit-db</a>. It took several tries to find a working
+          shellcode. We must then
+          write this hex code to an environment variable without it being considered as
+          merely a string, so we use the <code>printf</code> shell function. Export
+          the environment variable like so:
+        </p>
+      <CodeBlock language="shell">
+        {`export EGG=$(printf "\\x6a\\x0b\\x58\\x31\\xf6\\x56\\x68\\x2f\\x2f\\x73\\x68\\x68\\x2f\\x62\\x69\\x6e\\x89\\xe3\\x31\\xc9\\x89\\xca\\xcd\\x80")`}
+      </CodeBlock>
+      <p>
+        This will run the shell just like in Narnia 0, and we would just use <code>cat</code> to
+        obtain the password for Narnia2.
+      </p>
+      </section>
+      <section>
+        <h2>Research</h2>
+        <p>
+          What is shellcode? Shellcode is just a general term for an injected payload that will be
+          executed by a target program. The term shellcode does not mean it is related to shell 
+          commands. The term came from its background as a payload to create a shell for the
+          attacker to utilise. In relation to the shellcode shown, each hex number
+          represents an assembly operation. A good understanding could be earned if we consider a
+          small C code. For example, a small program printing hello world.
+        </p>
+        <CodeBlock language="c">
+          {`#include <stdio.h>
+
+int main(void) {
+  printf("Hello world");
+  return 0;
+}`}
+        </CodeBlock>
+        <p>
+          When this is compiled, we can see the hex representation of all the assembly
+          instructions using <code>objdump</code>. It is located at the centre, as seen
+          in the picture below.
+        </p>
+        <BlogImage src='/images/narnia1-1.png' alt='the hex representation of assembly instruction
+        located at the centre of the objdump result'/>
+        <p>
+          These are the operation codes to run the program of printing hello world.
+          The main problem with this is that, the usage of null bytes in a string could
+          terminate the string, this is undesirable product.Therefore, normally,
+          these opcode shellcodes are written directly using assembly or at least edited to
+          avoid operations with null bytes. It is also possible to find shellcodes already made by 
+          others, namely, in the website <a href='http://shell-storm.org/shellcode/'> shell-storm</a>. To read more about shellcode, I would
+          recommend reading about them in this, <a href='https://www.exploit-db.com/docs/english/13019-shell-code-for-beginners.pdf'>document written by exploit-db.com</a>
+        </p>
+        <p>
+          Another thing to consider was that since these shellcodes utilises hex representation
+          of operation codes, it is limited to the specific infrastructure that uses the
+          same operation codes.
         </p>
       </section>
-      <CodeBlock language="shell">
-        {`export EGG=$(python -c 'print("\\x31\\xc9\\xf7\\xe1\\x51\\xbf\\xd0\\xd0\\x8c\\x97\\xbe\\xd0\\x9d\\x96\\x91\\xf7\\xd7\\xf7\\xd6\\x57\\x56\\x89\\xe3\\xb0\\x0b\\xcd\\x80")')`}
-      </CodeBlock>
+      <section>
+        <h2>Reflection</h2>
+        <p>
+          This was quite a difficult CTF level, but I&apos;ve learnt a few things that
+          I had not known before. Namely that we may insert what seems to be just a string
+          of hex, when instead it is an fully executable program. That was quite interesting
+          to see. When doing buffer overflow attack, it at first seem limited to what
+          we can do. However, seeing how it is possible to write assembly instructions,
+          it reveals to me what a buffer overflow attack can fully accomplish. This
+          made me reconsider how I should perceive potential overflow problems.
+        </p>
+        <p>
+          In terms of completing the challenge, I did feel that I may have taken too long
+          in trying to figure out what I should insert in the environment variable.
+          While I still feel that requiring hints was a little dissapointing, at the
+          end I was able to find my own shellcode and assign it to the environment
+          variable with my own understanding of what I&apos;m trying to accomplish
+          and how everything should work.
+        </p>
+      </section>
       <SpoilerKey>{`nairiepecu`}</SpoilerKey>
       <PageNav link1='/narnia/narnia0' link2='/narnia/narnia2' />
     </div>
   )
 }
 export default narnia1
+
